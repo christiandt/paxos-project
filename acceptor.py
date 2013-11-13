5dc8572da8c61bd955707307badf6e56648f2eca
Kine Johnsrud <kinejohnsrud@gmail.com>
2013-11-12 16:04:04 -0800

    Added encoding declaration
    Added encoding declaration to make the comments actually visible.

--- acceptor.py
+++ acceptor.py
@@ -1,1 +1,17 @@
+# coding=utf-8
 __author__ = 'Kine'
+
+
+# Phase Prepare - COHORT
+
+# Upon receive (“prepare”, bal) from i
+# if bal ≥ BallotNum then
+# BallotNum ← bal
+# send (“ack”, bal, AcceptNum, AcceptVal) to i
+
+# Phase Accept - COHORT
+
+# Upon receive (“accept”, b, v) if b ≥ BallotNum then
+# AcceptNum ← b; AcceptVal ← v proposal */
+# ￼/* accept send (“accept”, b, v) to all (first time only)
+
